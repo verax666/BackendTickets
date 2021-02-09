2ff360e79ee51f5335a8b547982d245730c3ade9
Jona <alexandercorexp@gmail.com>
2021-02-09 10:03:50 -0600

    v1

--- models/ticket.model.js
+++ models/ticket.model.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, Sequelize) => {
             type: Sequelize.STRING
         },
         description: {
-            type: Sequelize.STRING
+            type: Sequelize.TEXT
         },
     }, { paranoid: true });
 
