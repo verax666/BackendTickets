18b2d27e99f9c0db905672868cf7c1b18a65a76f
Jona <alexandercorexp@gmail.com>
2021-03-30 12:13:23 -0600

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ const Vendedor = db.dbAltCte.vendedor;
 
 db.dbpreAlta.dbpreAlta.sync({ force: false }).then(() => {
   //console.log("Drop and re-sync db.");
+  bot.onText(/\/echo (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+
+    // send back the matched "whatever" to the chat
+    bot.sendMessage(chatId, resp);
+  });
 
 });
 // db.dbAltCte.dbAltCte.sync({ force: false }).then(() => {
