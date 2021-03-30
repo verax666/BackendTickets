8fd9b3446068a90d3913fd161796851019edc223
Jona <alexandercorexp@gmail.com>
2021-03-30 14:44:10 -0600

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ db.dbpreAlta.dbpreAlta.sync({ force: false }).then(() => {
       const resp = match[1]; // the captured "whatever"
       // send back the matched "whatever" to the chat
 
-      bot.sendMessage(chatId, "Id: " + JSON.stringify(r, null, 2) + "Nombre: " + JSON.stringify(r, null, 2) + "\n Porvafor Escriba /alta su id:\n Ejemplo /alta 3")
+      bot.sendMessage(chatId, "Id: " + JSON.stringify(r, null, 2) + "\n Porvafor Escriba /alta su id:\n Ejemplo /alta 3")
 
     });
   });
